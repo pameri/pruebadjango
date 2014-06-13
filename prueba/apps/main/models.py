5c9bad6990a6007dc8764930715f3a3a955bcd2f
Percy Ameri <percy_ameri@outlook.com>
2014-06-13 12:37:46 -0500

    nuevo model Proveedor

--- prueba/apps/main/models.py
+++ prueba/apps/main/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Proveedor(models.Model):
+    codigo = models.CharField(max_length=12, db_column='prov_codi', primary_key=True ,blank=True)
+    nombre = models.CharField(max_length=200)   
     
+    class Meta:
+        db_table = 'vca_proveedor'
+        
+        
 class Almacen(models.Model):
     codigo = models.CharField(max_length=12, db_column='alma_codi', primary_key=True ,blank=True)
     nombre = models.CharField(max_length=200)   
